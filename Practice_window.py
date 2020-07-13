ad5faa9e77fae81135f9b54f9e7ecaf5bfbea047
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2020-07-13 12:05:27 +0300

    Modified input tracking

--- Practice_window.py
+++ Practice_window.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import tkinter.messagebox
 import multiplication
 import memory_buffer
@@ -10,6 +11,7 @@ class PracticeWindow:
     button_color = '#1b4d4c'
     active_background_color = '#284C79'
     entry_background = '#1b4d4c'
+    do_tracking = False
 
     def window_for_practice(self, root):
 
@@ -39,24 +41,27 @@ class PracticeWindow:
             elif current_focus == self.entry_for_sign_y.winfo_id():
                 self.entry_for_mantissa_y.focus()
 
-        def focus_out(event):
-            if self.check_entry(event.widget):
-                event.widget["bg"] = "red"
-            else:
-                event.widget["bg"] = self.entry_background
+        def conf(event):
+            canvas.configure(scrollregion=canvas.bbox('all'))
 
-
+        def check_all(entries):
+            if self.do_tracking:
+                for entry in entries:
+                    if self.track_input(entry):
+                        entry["bg"] = "red"
+                    else:
+                        entry["bg"] = self.entry_background
+                root.after(31, lambda: check_all(entries))
 
         self.practice_window = Toplevel(root)
         self.practice_window.configure(background=self.background_color)
+        self.do_tracking = True
         # self.practice_window.resizable(width=False, height=False)
-
         main_menu = Menu(self.practice_window)
         main_menu.add_command(label="Головне меню")
         main_menu.add_command(label="Перейти до теорії")
         main_menu.add_command(label="Зберегти у файл")
         self.practice_window.config(menu=main_menu)
-
         left_part = Frame(self.practice_window, bg=self.background_color)
         right_part = Frame(self.practice_window, bg=self.background_color)
         left_part.pack(side=LEFT)
@@ -72,8 +77,7 @@ class PracticeWindow:
 
         self.option_variable = StringVar(frame_x)
         self.option_variable.set("1")
-
-        self.option_menu = OptionMenu(frame_x, self.option_variable, "1", "2", "3", "4", )
+        self.option_menu = OptionMenu(frame_x, self.option_variable, "1", "2", "3", "4")
         self.option_menu["background"] = self.entry_background
         self.option_menu["justify"] = "center"
         self.option_menu["activebackground"] = self.active_background_color
@@ -108,6 +112,12 @@ class PracticeWindow:
                            relief=FLAT)
         self.entry_for_mantissa_y = Entry(frame_y, bg=self.entry_background, fg=self.front_color, width=7, font=("", 13), justify=CENTER, relief=FLAT)
 
+        self.confirm_button = Button(frame_y, width=11, text="Порахувати", font=("Arial", 12), bg=self.button_color, fg=self.front_color,
+                                     activebackground=self.active_background_color, relief=GROOVE)
+        self.attention_img = Image.open(r"Photo/attention.png").resize((35, 35), Image.ANTIALIAS)
+        self.attention_img = ImageTk.PhotoImage(self.attention_img)
+        self.attention = Label(frame_y, image=self.attention_img, bg=self.background_color)
+
         self.method_of_operation.grid(row=0, column=0, padx=8, pady=20)
         self.option_menu.grid(row=0, column=1, padx=8, pady=10, sticky=W)
         order_x.grid(row=1, column=0, padx=8, pady=10)
@@ -124,9 +134,7 @@ class PracticeWindow:
         mantissa_y.grid(row=2, column=0, padx=8, pady=5)
         self.entry_for_mantissa_y.grid(row=2, column=1, padx=8, pady=10)
 
-        self.confirm_button = Button(left_part, width=11, text="Порахувати", font=("Arial", 12), bg=self.button_color, fg=self.front_color,
-                                     activebackground=self.active_background_color, relief=GROOVE)
-        self.confirm_button.pack()
+        self.confirm_button.grid(row=3, column=0, sticky=E, padx=8, pady=20)
 
         # right part
         frame_for_label = Frame(right_part, bg=self.background_color)
@@ -134,18 +142,25 @@ class PracticeWindow:
         self.label_of_current_operation = Label(frame_for_label, bg='#1b4d4c', fg="#65ffff", font=("Ubuntu", 20), text="CURRENT OPERATION")
         self.label_of_current_operation.pack(side=TOP, fill=X)
 
-        self.frame_for_text = Frame(right_part, bg=self.background_color)
-        self.frame_for_text.pack(fill=BOTH, pady=30)
+        frame_for_canvas = Frame(right_part, bg=self.background_color)
+        frame_for_canvas.pack(pady=30)
 
-        # text = Text(self.frame_for_text, width=75, height=20, font=("Times", 15), wrap=WORD, bg='#284C79', fg='#A9B7C6', relief=FLAT, yscrollcommand=set())
-        # scrollb_y = Scrollbar(self.frame_for_text, command=text.yview)
-        # text['yscrollcommand'] = scrollb_y.set
-        # scrollb_y.pack(fill=Y, side=RIGHT)
-        # text.pack(side=LEFT, fill=BOTH)
+        canvas = Canvas(frame_for_canvas, bg=self.background_color)
+        self.frame_for_text = Frame(canvas, bg=self.background_color)
+        myscrollbar1 = Scrollbar(frame_for_canvas, orient='vertical', command=canvas.yview)
+        canvas.configure(yscrollcommand=myscrollbar1.set)
+        myscrollbar1.pack(side=RIGHT, fill=Y)
+        myscrollbar2 = Scrollbar(frame_for_canvas, orient='horizontal', command=canvas.xview)
+        canvas.configure(xscrollcommand=myscrollbar2.set)
+        myscrollbar2.pack(side=BOTTOM, fill=X)
+        canvas.create_window((0, 0), window=self.frame_for_text, anchor='nw')
+        self.frame_for_text.bind('<Configure>', conf)
+        canvas.config(width=774, height=600)
+        canvas.pack(fill=BOTH)
 
-        frame_for_logo = Frame(right_part, bg='#1b4d4c')
-        frame_for_logo.pack(pady=10, side=BOTTOM, fill=BOTH)
-        logo = Label(frame_for_logo, font=('Ubuntu', 15), text="LOGO", bg='#002451', fg='#65ffff')
+        frame_for_logo = Frame(self.practice_window, bg=self.background_color)
+        frame_for_logo.pack(pady=10, side=BOTTOM, fill=X)
+        logo = Label(frame_for_logo, font=('Times', 15), text="RedButton", bg=self.background_color, fg=self.front_color)
         logo.pack(side=RIGHT)
 
         #  binding
@@ -163,47 +178,55 @@ class PracticeWindow:
         self.entry_for_sign_y.bind("<Up>", on_up)
         self.entry_for_mantissa_y.bind("<Up>", on_up)
 
-        self.entry_for_order_x.bind("<FocusOut>", focus_out)
-        self.entry_for_sign_x.bind("<FocusOut>", focus_out)
-        self.entry_for_mantissa_x.bind("<FocusOut>", focus_out)
-        self.entry_for_order_y.bind("<FocusOut>", focus_out)
-        self.entry_for_sign_y.bind("<FocusOut>", focus_out)
-        self.entry_for_mantissa_y.bind("<FocusOut>", focus_out)
+        self.entries = [self.entry_for_order_x, self.entry_for_sign_x, self.entry_for_mantissa_x,
+                        self.entry_for_order_y, self.entry_for_sign_y, self.entry_for_mantissa_y]
+        check_all(self.entries)
 
     def multiplication_window(self, root):
-        def multiply():
-            self.destroy_labels()
-            multiplication.calculate((int(self.option_variable.get()) - 1),
-                                     self.entry_for_order_x.get(),
-                                     self.entry_for_order_y.get(),
-                                     self.entry_for_sign_x.get(),
-                                     self.entry_for_sign_y.get(),
-                                     self.entry_for_mantissa_x.get(),
-                                     self.entry_for_mantissa_y.get())
 
-            for i in range(len(memory_buffer.memory[0])):
-                for j in range(len(memory_buffer.memory[0][i])):
-                    display_label = Label(self.frame_for_text, text="\n", bg="#1b4d4c", fg='#65ffff', font=("times", 20), justify=CENTER,
-                                          relief=FLAT)
-                    display_label["text"] = memory_buffer.memory[0][i][j]
-                    display_label.grid(row=i + 1, column=j + 1, padx=3, pady=4)
-                    self.result_labels.append(display_label)
+        def multiply():
+            if self.check_entries(6):
+                self.destroy_labels()
+                self.attention.grid_forget()
+            else:
+                print("Nothing")
+
+            # multiplication.calculate((int(self.option_variable.get()) - 1),
+            #                          self.entry_for_order_x.get(),
+            #                          self.entry_for_order_y.get(),
+            #                          self.entry_for_sign_x.get(),
+            #                          self.entry_for_sign_y.get(),
+            #                          self.entry_for_mantissa_x.get(),
+            #                          self.entry_for_mantissa_y.get())
+            #
+            # for i in range(len(memory_buffer.memory[0])):
+            #     for j in range(len(memory_buffer.memory[0][i])):
+            #         display_label = Label(self.frame_for_text, text="\n", bg="#1b4d4c", fg='#65ffff', font=("times", 20), justify=CENTER,
+            #                               relief=FLAT)
+            #         display_label["text"] = memory_buffer.memory[0][i][j]
+            #         display_label.grid(row=i + 1, column=j + 1, padx=3, pady=4)
+            #         self.result_labels.append(display_label)
 
         self.check_existence_of_window(root)
         self.result_labels = []
+        self.method_of_operation.grid(row=0, column=0, padx=8, pady=20)
+        self.option_menu.grid(row=0, column=1, padx=8, pady=10, sticky=W)
         self.practice_window.title("Операція множення")
         self.label_of_current_operation["text"] = "Множення двійкових чисел"
         self.confirm_button["command"] = multiply
         self.method_of_operation["text"] = "Спосіб множення"
-
         self.reset_table()
         self.practice_window.protocol("WM_DELETE_WINDOW", self.on_closing_practice_window)
 
     def division_window(self, root):
-        self.check_existence_of_window(root)
+        def divide():
+            pass
 
+        self.check_existence_of_window(root)
         self.practice_window.title("Операція ділення")
         self.label_of_current_operation["text"] = "Ділення двійкових чисел"
+        self.confirm_button["command"] = divide
+        self.method_of_operation["text"] = "Спосіб ділення"
 
         self.reset_table()
         self.practice_window.protocol("WM_DELETE_WINDOW", self.on_closing_practice_window)
@@ -222,6 +245,8 @@ class PracticeWindow:
 
         self.practice_window.title("Операція добування корення")
         self.label_of_current_operation["text"] = "Добування корення"
+        self.method_of_operation.grid_forget()
+        self.option_menu.grid_forget()
 
         self.reset_table()
         self.practice_window.protocol("WM_DELETE_WINDOW", self.on_closing_practice_window)
@@ -242,7 +267,7 @@ class PracticeWindow:
         except AttributeError:
             self.window_for_practice(root)
 
-    def check_entry(self, entry):
+    def track_input(self, entry):
         # 400 - not int
         # 401 - negative
         # 402- there are numbers bigger than 1
@@ -250,14 +275,10 @@ class PracticeWindow:
         bigger_than_1 = False
         errors = []
         value = entry.get()
-        if value == "":
-            pass
-
-        else:
+        if not value == "":
             try:
                 if int(value) < 0:
                     errors.append(401)
-                    # print("401 negative")
                 else:
                     if 401 in errors:
                         errors.remove(401)
@@ -272,7 +293,6 @@ class PracticeWindow:
                     errors.remove(400)
 
                 if bigger_than_1:
-                    # print("402 bigger than 1")
                     errors.append(402)
                 else:
                     if 402 in errors:
@@ -284,12 +304,8 @@ class PracticeWindow:
                             errors.remove(403)
                     else:
                         errors.append(403)
-                        # print("403 sign troubles")
-
             except ValueError:
                 errors.append(400)
-                # print("400 not int")
-        # print(errors)
         return errors
 
     def reset_table(self):
@@ -321,3 +337,12 @@ class PracticeWindow:
     def on_closing_practice_window(self):
         self.practice_window.destroy()
         del self.practice_window
+        self.do_tracking = False
+
+    def check_entries(self, quantity):
+        ok = True
+        for entry in self.entries[:quantity]:
+            if entry["bg"] == "red" or entry.get() == "":
+                ok = False
+                self.attention.grid(row=3, column=1, sticky=W, padx=8, pady=20)
+        return ok
